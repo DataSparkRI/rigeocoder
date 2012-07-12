9cd61a0201a64517eaa5d09e50568e8dd921d16e
Angel Medrano <asmedrano@gmail.com>
2012-07-12 16:29:27 -0400

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+    name='rigeocoder',
+    version='0.9',
+    license="BSD",
+    
+    install_requires = ["geopy>=0.94.2"],
+
+    description = 'Rhode Island Geocoding with URI + google fallback',
+
+    author='Provplan',
+    author_email='amedrano@provplan.org',
+    url='https://github.com/ProvidencePlan/rigeocoder',
+    download_url='https://github.com/ProvidencePlan/rigeocoder.git',
+    include_package_data = True,
+
+    zip_safe=False,
+    classifiers=[
+	    'Development Status::4- Beta',
+	    'Environment :: Web Environment',
+	    'Intended Audience :: ',
+	    'License :: BSD License',
+	    'Operating System :: OS independent',
+	    'Programming Language :: Python',
+    ]
+)
