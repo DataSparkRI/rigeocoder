e53fc5898a8ceabce416fac5e3b2b3f368cf0c57
Angel Medrano <asmedrano@gmail.com>
2013-02-01 11:16:30 -0500

    updates format of returned results

--- rigeocoder.py
+++ rigeocoder.py
@@ -7,15 +7,18 @@ wkid = '4269' #GCS_North_American_1983
 out_form = 'json'
 
 def geocode_address_uri(street, city, zip_code = ''):
-	urllib.quote_plus(street)
-	urllib.quote_plus(city)
-	req_url = "%s?Street=%s&City=%s&ZIP=%s&outSR=%s&f=%s" % (base_url, street, city, zip_code, wkid, out_form)
-	raw_data = urllib.urlopen(req_url)
-	json_data = json.loads(raw_data.read())
-
-	if 'candidates' in json_data: #address was geocoded
-		if json_data['candidates']:
-			return (json_data['candidates'][0]['location']['y'], json_data['candidates'][0]['location']['x'])
+    urllib.quote_plus(street)
+    urllib.quote_plus(city)
+    req_url = "%s?Street=%s&City=%s&ZIP=%s&outSR=%s&f=%s" % (base_url, street, city, zip_code, wkid, out_form)
+    raw_data = urllib.urlopen(req_url)
+    json_data = json.loads(raw_data.read())
+    results = []
+    if 'candidates' in json_data: #address was geocoded
+        if json_data['candidates']:
+            for cand in json_data['candidates']:
+                if float(cand['score']) > 80:
+                    results.append((cand['address'], (cand['location']['y'], cand['location']['x'])))
+            return results
 
 	return None
 
@@ -33,8 +36,7 @@ def geocode_address_dotus(street, city, zip_code=''):
     address = "%s %s, %s" % (street, city, zip_code)
     try:
         us = geocoders.GeocoderDotUS()
-        place, (lat, lng) = us.geocode(address)
-        return (lat,lng)
+        return list(us.geocode(address, exactly_one=False))
     except Exception:
         return None
 
